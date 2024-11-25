38ad1c78affef4440bece2b38e295de0a1d0f389
Anuj Gupta <anujgupta4155@gmail.com>
2024-11-25 19:36:12 +0530

    chore: Setup mantine-ui, zod and useform

---
+++ src/mantine.config.ts
@@ -0,0 +1,33 @@
+import { MantineThemeOverride, Input } from "@mantine/core";
+import classes from "../src/global.module.css";
+export const theme: MantineThemeOverride = {
+  components: {
+    Anchor: {
+      styles: {
+        root: {
+          fontWeight: 700, // Set global font weight for all anchors
+          color: "#1098AD", // Set global text color for all anchors
+        },
+      },
+    },
+    Button: {
+      defaultProps: {
+        color: "#1098AD",
+        h: "56",
+        size: "xl",
+        // Set default button color
+      },
+    },
+    Input: Input.extend({
+      classNames: {
+        input: classes.input,
+      },
+    }),
+
+    NumberInput: {
+      defaultProps: {
+        size: "md", // Set default button color
+      },
+    },
+  },
+};
