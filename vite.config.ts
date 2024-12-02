30606a760f70269fe0ae5c435e17dbe79b07cc5f
Anuj Gupta <anujgupta4155@gmail.com>
2024-12-02 15:45:30 +0530

    chore: Add alias for src folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src", // Alias @ to the src folder
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
